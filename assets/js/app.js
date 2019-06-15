3a8d7f449c0b9073da5488e56f4944fc725c9881
Gustavo Fournier <gustavo.s.fournier@gmail.com>
2019-06-15 03:40:11 -0300

    Consume API to generate seats details in the Cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,52 @@
 import 'normalize.css';
+import $ from 'jquery';
 require('../css/app.css');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+$(document).ready(function() {
+    
+    // Ao clicar no card, obter mais detalhes na API e montar HTML
+    $('.card').on('click', function() {
+        var card = $(this);
+        var tripId = card.attr('data-id');
+
+        card.addClass('open');
+        card.find('.seats').show();
+
+        createSeats(tripId);
+    });
+    
+    // Função para obter os bancos disponíveis de uma certa viagem
+    function createSeats(travelId) {
+        var baseAPI = "https://clickbus-teste.herokuapp.com/trip/";
+        var busURL = baseAPI + travelId;
+
+        $.get(busURL, function(res){
+            let seats = JSON.parse(res);
+            createSeatsView(travelId, seats.map);
+        });
+    }
+
+    // Criar os assentos na view de acordo com as informações da API
+    function createSeatsView(id, seats) {
+
+        for (let i = 0; i < seats.length; i++) {
+            let seat = $("<li class='seat'></li>");
+            let label = seats[i].label;
+            let available = seats[i].available;
+            let x = seats[i].position.x;
+            let y = seats[i].position.y;
+
+            if (available == true) {
+                seat.append("<span>"+label+"</span>");
+                seat.addClass("available");
+            } else {
+                seat.addClass("busy");
+            }
+            seat.attr("x", x);
+            seat.attr("y", y);
+
+            $('.card[data-id='+id+'] .seats .bus-seats').append(seat);
+        }
+        
+    }
+});
